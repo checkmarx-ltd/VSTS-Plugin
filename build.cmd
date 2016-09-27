5a0bdf4346fc278e858e6da16a43ccd7b4f006f9
iland <ilan.dayan@checkmarx.com>
2016-09-27 14:29:58 +0300

    bugid: 00 CR_by: n/a

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 del *.vsix
 #tsc
 tfx extension create --manifest-globs vss-extension.json
-#tfx extension publish --share-with ilandayan --token ***REMOVED***
+#tfx extension publish --share-with Checkmarx --token ***REMOVED***
 echo | pause
