1c75725e0ee012e849a9413b176bc6fd316e4e2b
Ken McDonald <ken.mcdonald@checkmarx.com>
2020-12-10 16:24:51 -0500

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 del *.vsix
 #tsc
 tfx extension create --bypass-validation --manifest-globs vss-extension.json
-#tfx extension publish --share-with Checkmarx --token x6msals6q6s7joqejd3od63xadccjfa374p5br7moorlmlhgwyra
+#tfx extension publish --share-with Checkmarx --token <TOKEN>
 echo | pause
