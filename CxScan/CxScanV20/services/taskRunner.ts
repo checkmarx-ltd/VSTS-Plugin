49fac496a3b39218382b35dcc2f69d50bf544abf
Majd <majd.mahajenah@checkmarx.com>
2020-12-20 18:39:39 +0200

    handle empty resolve

--- CxScan/CxScanV20/services/taskRunner.ts
+++ CxScan/CxScanV20/services/taskRunner.ts
@@ -107,7 +107,7 @@ export class TaskRunner {
                 if (err) {
                     reject(err);
                 } else {
-                    resolve();
+                    resolve(jsonReportPath);
                 }
             });
         });
