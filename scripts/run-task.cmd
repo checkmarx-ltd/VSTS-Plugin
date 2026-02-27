c9df757caa3e464f0f03bd9e7081a3184c1b5419
Riyaj Shaikh <227867992+cx-riyaj-shaikh@users.noreply.github.com>
2026-02-27 11:15:29 +0530

    For Testing Purpose
    For Testing Purpose

--- scripts/run-task.cmd
+++ scripts/run-task.cmd
@@ -1,44 +1,60 @@
+@echo off
 pushd %~dp0..\CxScan\CxScanV20
 
-call tsc
+:: --- Build TypeScript ---
+call npx tsc
 
+:: --- SAST Connection / Auth ---
 set INPUT_ENABLEPROXY=false
-
 set INPUT_CheckmarxService=endpointId
-set INPUT_PROXYURL=localhost:8080
-set ENDPOINT_URL_endpointId=URL
-set ENDPOINT_AUTH_PARAMETER_endpointId_USERNAME=user
-set ENDPOINT_AUTH_PARAMETER_endpointId_PASSWORD=pass
-set BUILD_SOURCESDIRECTORY=C:\Checkmarx\powershellPlugin
-set INPUT_ENABLESASTSCAN=false
-set INPUT_PROJECTNAME=newOne12
-set INPUT_FULLTEAMNAME=\CxServer
-set INPUT_DENYPROJECT=false
-set INPUT_INCSCAN=true
-set INPUT_COMMENT=Greetings from TypeScript
+set ENDPOINT_URL_endpointId=http://10.33.0.40
 set ENDPOINT_AUTH_SCHEME_endpointId=UsernamePassword
-set BUILD_DEFINITIONNAME=builddef
-set BUILD_BUILDNUMBER=23
-set INPUT_SYNCMODE=true
-set INPUT_ENABLEPOLICYVIOLATIONS=true
-set INPUT_PRESET=Checkmarx Default
+set ENDPOINT_AUTH_PARAMETER_endpointId_USERNAME=admin@cx
+set ENDPOINT_AUTH_PARAMETER_endpointId_PASSWORD=Cx12345678!
 
-set INPUT_VULNERABILITYTHRESHOLD=false
-set INPUT_HIGH=1
-set INPUT_MEDIUM=1
-set INPUT_LOW=1
-set ENDPOINT
-
-set INPUT_ENABLEDEPENDENCYSCAN=true
+:: SCA CONNECTION
 set INPUT_dependencyServerURL=endpointIdSCA
-set ENDPOINT_URL_endpointIdSCA=scaapi
-set ENDPOINT_AUTH_PARAMETER_endpointIdSCA_USERNAME=user
-set ENDPOINT_AUTH_PARAMETER_endpointIdSCA_PASSWORD=pass
+set ENDPOINT_URL_endpointIdSCA=https://api-sca.checkmarx.net
 set ENDPOINT_AUTH_SCHEME_endpointIdSCA=UsernamePassword
-set INPUT_DEPENDENCYACCESSCONTROLURL=scaurl
-set INPUT_DEPENDENCYWEBAPPURL=scaurl
-set INPUT_DEPENDENCYTENANT=tenant
-set Endpoint
-node target\index.js
+set ENDPOINT_AUTH_PARAMETER_endpointIdSCA_USERNAME=UmeshW
+set ENDPOINT_AUTH_PARAMETER_endpointIdSCA_PASSWORD=DEVcx78$
+set INPUT_DEPENDENCYACCESSCONTROLURL=https://platform.checkmarx.net
+set INPUT_DEPENDENCYWEBAPPURL=https://sca.checkmarx.net
+set INPUT_DEPENDENCYTENANT=plugins
+set INPUT_SCATEAM=/CxServer
+set INPUT_SYNCMODE=true
 
-popd
+:: --- Project Context ---
+set INPUT_PROJECTNAME=RegressionTest
+set INPUT_FULLTEAMNAME=/CxServer
+set INPUT_OVERRIDEPROJECTSETTINGS=true
+
+:: --- Preset + Engine Config ---
+set INPUT_PRESET=Checkmarx Default
+set INPUT_CONFIGURATION=Default Configuration
+set INPUT_CONFIGURATIONID=1
+set INPUT_ENGINECONFIGURATIONID=1
+set INPUT_ENGINECONFIGID=1
+set INPUT_ENGINECONFIGURATION=Default Configuration
+
+:: --- Source Under Test ---
+set BUILD_SOURCESDIRECTORY=C:\Project\NodeGoat-master\NodeGoat-master
+
+:: --- Scan Toggles ---
+set INPUT_enableSastScan=false
+set INPUT_ENABLEDEPENDENCYSCAN=true
+set INPUT_ISENABLESCARESOLVER=true
+set SYSTEM_DEBUG=true
+
+@REM set INPUT_INCLUDESOURCE=true
+@REM set INPUT_DEPENDENCYFILEEXTENSION=
+@REM set INPUT_DEPENDENCYFOLDEREXCLUSION=src,node_modules,bin,obj
+
+set INPUT_PATHTOSCARESOLVER=C:\tools\CxSCAResolver
+set INPUT_SCARESOLVERADDPARAMETERS=-s C:\2738\VulnerableDockerfile-main -n TestLocal -r C:\2738\zendphp\results-sca.json
+
+
+:: --- Execute Scan ---
+"C:\Program Files\nodejs\node.exe" target\index.js
+
+popd    
